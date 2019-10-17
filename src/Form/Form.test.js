19a8361078ce874cf4f1842cc06db5ff611ad9d4
Julian Enochs <julianenochs@yahoo.com>
2019-10-17 10:10:34 -0600

    Switch to testing

---
+++ src/Form/Form.test.js
@@ -0,0 +1,8 @@
+import Form from './Form';
+import { isTSAnyKeyword } from '@babel/types';
+
+describe('Form', () => {
+    it('should match the snapshot with all of the data passed through', () => {
+        
+    })
+})
