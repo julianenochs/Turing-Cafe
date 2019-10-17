fa0336b5a4044687719c0c76b644afca2097a20e
Julian Enochs <julianenochs@yahoo.com>
2019-10-17 09:57:53 -0600

    Add state change to form component

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import './Form.css';
 
-class Form extends Component {
+export default class Form extends Component {
     constructor() {
         super();
         this.state = {
-
+            name: '',
+            date: '',
+            time: '',
+            numberOfGuests: 0
         }
     }
 
     componentDidMount() {
 
+    }
+
+    handleChange = (e) => {
+        this.setState({ [e.target.name] : e.target.value })
     }
 
     render() {
@@ -19,27 +26,29 @@ class Form extends Component {
                 <input 
                     type='text'
                     name='name'
-                    // value={}
+                    onChange={this.handleChange}
+                    value={this.state.name}
                     placeholder='Name'/>
                 <input
                     type='date'
-                    name='name'
-                    // value={}
+                    name='date'
+                    onChange={this.handleChange}
+                    value={this.state.date}
                     placeholder='Date' />
                 <input
                     type='time'
-                    name='name'
-                    // value={}
+                    name='time'
+                    onChange={this.handleChange}
+                    value={this.state.time}
                     placeholder='Time' />
                 <input
-                    type='text'
-                    name='name'
-                    // value={}
+                    type='number'
+                    name='numberOfGuests'
+                    onChange={this.handleChange}
+                    value={this.state.numberOfGuests}
                     placeholder='Number of Guests' />
                 <button>Make Reservation</button>
             </section>
         )
     }
 }
-
-export default Form;
